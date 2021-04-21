c69019e87e770133dddc711472fb0d338a27252a
PierreSQS <nomo_messina@web.de>
2021-04-21 16:20:37 +0200

    Initial Project Setup

---
+++ springboot-multi/springboot-multi-webapp/src/main/java/org/sonatype/mavenbook/SpringbootMultiWebAppApplication.java
@@ -0,0 +1,12 @@
+package org.sonatype.mavenbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultiWebAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultiWebAppApplication.class, args);
+    }
+
+}
---
+++ springboot-multi/springboot-multi-webapp/src/test/java/org/sonatype/mavenbook/SpringbootMultiWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonatype.mavenbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultiWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
