9076d4fd6ab7a32c227db5ca38d9b6c5d705d21d
PierreSQS <nomo_messina@web.de>
2021-04-24 02:01:08 +0200

    Commit07: Fixed the SAX-Parser

--- springboot-multi/springboot-multi-weather/src/main/java/org/sonatype/mavenbook/weather/YahooParser.java
+++ springboot-multi/springboot-multi-weather/src/main/java/org/sonatype/mavenbook/weather/YahooParser.java
@@ -29,31 +29,32 @@ public class YahooParser {
 	
 	log.info( "Parsing XML Response" );
 	Location location = new Location();
-	location.setCity( doc.valueOf("/rss/channel/y:location/@city") );
-	location.setRegion( doc.valueOf("/rss/channel/y:location/@region") );
-	location.setCountry( doc.valueOf("/rss/channel/y:location/@country") );
+	location.setCity( doc.valueOf("/rss/channel/yweather:location/@city") );
+	location.setRegion( doc.valueOf("/rss/channel/yweather:location/@region") );
+	location.setCountry( doc.valueOf("/rss/channel/yweather:location/@country") );
+	log.info("*************** the location: {} ************", location.getCity());
 	weather.setLocation( location );
 
 	Condition condition = new Condition();
-	condition.setText( doc.valueOf("/rss/channel/item/y:condition/@text") );
-	condition.setTemp( doc.valueOf("/rss/channel/item/y:condition/@temp") );
-	condition.setCode( doc.valueOf("/rss/channel/item/y:condition/@code") );
-	condition.setDate( doc.valueOf("/rss/channel/item/y:condition/@date") );
+	condition.setText( doc.valueOf("/rss/channel/item/yweather:condition/@text") );
+	condition.setTemp( doc.valueOf("/rss/channel/item/yweather:condition/@temp") );
+	condition.setCode( doc.valueOf("/rss/channel/item/yweather:condition/@code") );
+	condition.setDate( doc.valueOf("/rss/channel/item/yweather:condition/@date") );
 	condition.setWeather( weather );
 	weather.setCondition( condition );
 
 	Atmosphere atmosphere = new Atmosphere();
-	atmosphere.setHumidity( doc.valueOf("/rss/channel/y:atmosphere/@humidity") );
-	atmosphere.setVisibility( doc.valueOf("/rss/channel/y:atmosphere/@visibility") );
-	atmosphere.setPressure( doc.valueOf("/rss/channel/y:atmosphere/@pressure") );
-	atmosphere.setRising( doc.valueOf("/rss/channel/y:atmosphere/@rising") );
+	atmosphere.setHumidity( doc.valueOf("/rss/channel/yweather:atmosphere/@humidity") );
+	atmosphere.setVisibility( doc.valueOf("/rss/channel/yweather:atmosphere/@visibility") );
+	atmosphere.setPressure( doc.valueOf("/rss/channel/yweather:atmosphere/@pressure") );
+	atmosphere.setRising( doc.valueOf("/rss/channel/yweather:atmosphere/@rising") );
 	atmosphere.setWeather( weather );
 	weather.setAtmosphere( atmosphere );
 
 	Wind wind = new Wind();
-	wind.setChill( doc.valueOf("/rss/channel/y:wind/@chill") );
-	wind.setDirection( doc.valueOf("/rss/channel/y:wind/@direction") );
-	wind.setSpeed( doc.valueOf("/rss/channel/y:wind/@speed") );
+	wind.setChill( doc.valueOf("/rss/channel/yweather:wind/@chill") );
+	wind.setDirection( doc.valueOf("/rss/channel/yweather:wind/@direction") );
+	wind.setSpeed( doc.valueOf("/rss/channel/yweather:wind/@speed") );
 	wind.setWeather( weather );
 	weather.setWind( wind );
 
@@ -64,7 +65,7 @@ public class YahooParser {
     
     private SAXReader createXmlReader() {
 	Map<String,String> uris = new HashMap<>();
-        uris.put( "y", "http://xml.yahoo.com/ns/rss/1.0" );
+        uris.put( "yweather", "http://xml.weather.yahoo.com/ns/rss/1.0" );
         
         DocumentFactory factory = new DocumentFactory();
         factory.setXPathNamespaceURIs( uris );
