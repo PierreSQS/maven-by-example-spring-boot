20f2606253d2db0c7cbe9f0ef8be7f675fe721f6
PierreSQS <nomo_messina@web.de>
2021-05-07 12:33:32 +0200

    Commit09-wf NOK: Attempt to use a Resource loader.

--- springboot-multi/springboot-multi-weather/src/test/java/org/sonatype/mavenbook/weather/YahooParserTest.java
+++ springboot-multi/springboot-multi-weather/src/test/java/org/sonatype/mavenbook/weather/YahooParserTest.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.sonatype.mavenbook.model.Weather;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
+
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @SpringBootTest(classes = {YahooParser.class})
@@ -23,26 +23,29 @@ class YahooParserTest {
 
     private String content;
 
-    @BeforeEach
-    void setUp() {
-        InputStream nyData =
-                getClass().getClassLoader().getResourceAsStream("ny-weather.xml");
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(nyData));
-        content = bufferedReader.lines().collect(Collectors.joining("\n"));
+    @Autowired //possible because of @BootStrapWith Annotation!!!
+    private ResourceLoader resourceLoader;
 
+    @BeforeEach
+    void setUp() throws IOException {
+        Resource weatherFileResource = resourceLoader.getResource("ny-weather.xml");
+
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(weatherFileResource.getInputStream()));
+        content = bufferedReader.lines().collect(Collectors.joining("\n"));
     }
 
     @Test
     void testParser() throws Exception {
 
-        log.debug("the Message to parse:{} {}","\n",content);
+        log.debug("the Message to parse:{} {}", "\n", content);
         Weather weather = new YahooParser().parseString(content);
-        assertEquals( "New York", weather.getLocation().getCity() );
-        assertEquals( " NY", weather.getLocation().getRegion() );
-        assertEquals( "United States", weather.getLocation().getCountry() );
-        assertEquals( "58", weather.getCondition().getTemp() );
-        assertEquals( "Sunny", weather.getCondition().getText() );
-        assertEquals( "55", weather.getWind().getChill() );
-        assertEquals( "44", weather.getAtmosphere().getHumidity() );
+        assertEquals("New York", weather.getLocation().getCity());
+        assertEquals(" NY", weather.getLocation().getRegion());
+        assertEquals("United States", weather.getLocation().getCountry());
+        assertEquals("58", weather.getCondition().getTemp());
+        assertEquals("Sunny", weather.getCondition().getText());
+        assertEquals("55", weather.getWind().getChill());
+        assertEquals("44", weather.getAtmosphere().getHumidity());
     }
 }
